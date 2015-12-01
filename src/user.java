3dd8daab410e28e8e14089605106bbf0917d764f
gquerbes <gquerbes@gmail.com>
2015-12-01 18:44:45 -0500

    start of changes for final

---
+++ src/user.java
@@ -0,0 +1,15 @@
+
+public abstract class user {
+
+	private String firstName;
+    private String lastName;
+    private String gNumber;
+    private String password;
+    private String phoneNumber;
+    private String email;
+    private String username;
+    
+    
+	
+	
+}
